623d3da1338902d50746de8d87828a4169748b85
Pavel Lobashov <shockwavenn@gmail.com>
2022-01-17 18:14:36 +0300

    Fix more

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,7 +2,6 @@
 
 require 'redis_to_telegram'
 
-
 RSpec.configure do |config|
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
