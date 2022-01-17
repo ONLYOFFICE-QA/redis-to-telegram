b1365484e0a128dcaf05903b42e66d71e61ba42b
Pavel Lobashov <shockwavenn@gmail.com>
2022-01-17 18:11:34 +0300

    Try to fix it again

--- spec/redis_to_telegram/redis_message_fetcher_spec.rb
+++ spec/redis_to_telegram/redis_message_fetcher_spec.rb
@@ -3,7 +3,7 @@
 require 'spec_helper'
 
 describe RedisToTelegram::RedisMessageFetcher do
-  let(:redis_message_fetcher) { described_class.new(host: 'redis') }
+  let(:redis_message_fetcher) { described_class.new(host: ENV['REDIS_HOST']) }
 
   it 'return empty array for empty redis connection' do
     expect(redis_message_fetcher.fetch_messages).to eq([])
