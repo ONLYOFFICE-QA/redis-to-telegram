6cd32c7f10071733c79a5a8172253b5654c2ba9c
Pavel Lobashov <shockwavenn@gmail.com>
2022-01-18 11:08:51 +0300

    Fix rubocop

--- spec/redis_to_telegram/redis_message_fetcher_spec.rb
+++ spec/redis_to_telegram/redis_message_fetcher_spec.rb
@@ -3,7 +3,7 @@
 require 'spec_helper'
 
 describe RedisToTelegram::RedisMessageFetcher do
-  let(:config) { RedisToTelegram::ConfigHelper.new('spec/data/fake_config.yml')}
+  let(:config) { RedisToTelegram::ConfigHelper.new('spec/data/fake_config.yml') }
   let(:redis_message_fetcher) { described_class.new(config: config) }
   let(:messages) {  redis_message_fetcher.fetch_messages }
 
@@ -12,14 +12,14 @@ describe RedisToTelegram::RedisMessageFetcher do
   end
 
   it 'return array of messages after some data push' do
-    push_message_to_redis({notification: 'text', chat: 'fake_telegram_chat_id'})
-    expect(messages.first).to eq(RedisToTelegram::Message.new('text', -12345))
+    push_message_to_redis({ notification: 'text', chat: 'fake_telegram_chat_id' })
+    expect(messages.first).to eq(RedisToTelegram::Message.new('text', -12_345))
   end
 
   it 'return array of message even if there is nil in middle' do
-    push_message_to_redis({notification: 'text', chat: 'fake_telegram_chat_id'})
+    push_message_to_redis({ notification: 'text', chat: 'fake_telegram_chat_id' })
     push_message_to_redis({})
-    push_message_to_redis({notification: 'text2', chat: 'fake_telegram_chat_id'})
+    push_message_to_redis({ notification: 'text2', chat: 'fake_telegram_chat_id' })
     expect(messages.length).to eq(3)
   end
 end
